d5cda996406c7c67ee787f897a41093c17a9d18a
huzs <huzs@163.com>
2021-11-06 22:04:11 +0800

    租户ID支持

--- smile-orm/src/test/java/org/smile/ormdb/dao/UpdateInfo.java
+++ smile-orm/src/test/java/org/smile/ormdb/dao/UpdateInfo.java
@@ -2,8 +2,9 @@ package org.smile.ormdb.dao;
 
 import java.util.Date;
 
+import lombok.Data;
 import org.smile.orm.ann.Column;
-
+@Data
 public class UpdateInfo {
 	@Column
 	private String createUser;
@@ -13,30 +14,5 @@ public class UpdateInfo {
 	private Date createTime;
 	@Column
 	private Date updateTime;
-	public String getCreateUser() {
-		return createUser;
-	}
-	public void setCreateUser(String createUser) {
-		this.createUser = createUser;
-	}
-	public String getUpdateUser() {
-		return updateUser;
-	}
-	public void setUpdateUser(String updateUser) {
-		this.updateUser = updateUser;
-	}
-	public Date getCreateTime() {
-		return createTime;
-	}
-	public void setCreateTime(Date createTime) {
-		this.createTime = createTime;
-	}
-	public Date getUpdateTime() {
-		return updateTime;
-	}
-	public void setUpdateTime(Date updateTime) {
-		this.updateTime = updateTime;
-	}
-	
-	
+
 }
